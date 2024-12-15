5277de78ca84d7fdfe294b343e6f0c1671fe50c7
kolasyam <syammanikantakola426@gmail.com>
2024-12-15 15:44:01 +0530

    projects

--- src/app/Components/Projects.tsx
+++ src/app/Components/Projects.tsx
@@ -7,7 +7,7 @@ import { IoCodeSlash } from "react-icons/io5";
 import { DiGithubFull } from "react-icons/di";
 import todo from "/projects/todo-app.jpg"
 import dicegame from '/projects/dice-game.jpg';
-import calculator from '/projects/calculator.jpg'
+import calculator from '/projects/geminiai.png'
 import AOS from 'aos'
 import 'aos/dist/aos.css';
 import Link from 'next/link';
@@ -91,7 +91,7 @@ export default function Projects(){
                         <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">html</div>
                     </div>
                     <p className="leading-relaxed mb-3">
-                     This weather app utilizes the AccuWeather API to fetch accurate weather data and presents it in a user-friendly interface. The application is responsive, ensuring seamless functionality and optimal user experience across both mobile and desktop devices.
+                    The Dice game project is a two-player game that allows users to roll dice and compete against each other. It incorporates interactive gameplay elements, where each player takes turns rolling the dice and accumulating points. The project showcases my ability to implement simple game logic and enhance user engagement.
                     </p>
                     <div className="flex justify-center flex-wrap">
                     <Link href="https://github.com/kolasyam/Dice-Game" passHref target="_blank">
@@ -110,17 +110,24 @@ export default function Projects(){
                     </div>
                 <div className="p-6 mt-7">
                     <h1 className="title-font text-lg font-medium text-gray-900 mb-2">
-                        Calculator
+                        GeminiAI API chatbot
                     </h1>
-                    <div className='flex flex-row justify-between items-center flex-wrap'>
+                    {/* <div className='flex flex-row justify-between items-center flex-wrap'>
                         <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700 mr-10">reactjs</div>
                         <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">Bootstrap</div>
+                        
+                    </div> */}
+                    <div className='flex flex-row justify-between items-center flex-wrap'>
+                        <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">Tailwindcss</div>
+                        <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">Nextjs</div>
+                        <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">Nypescript</div>
+                        <div className="text-white bg-indigo-500 rounded-md px-2 hover:bg-indigo-700">Supabase</div>
                     </div>
                     <p className="leading-relaxed mb-3">
-                     This weather app utilizes the AccuWeather API to fetch accurate weather data and presents it in a user-friendly interface. The application is responsive, ensuring seamless functionality and optimal user experience across both mobile and desktop devices.
+                    GeminiAI is an AI-powered chatbot built using Supabase, Next.js, and TailwindCSS, leveraging advanced natural language processing for dynamic conversations. The project integrates an API to provide real-time, context-aware responses across various domains. It demonstrates my proficiency in AI, API integration, and modern front-end development.
                     </p>
                     <div className="flex justify-center flex-wrap">
-                    <Link href="https://github.com/kolasyam/Calculator-App" passHref target="_blank">
+                    <Link href="https://github.com/kolasyam/gemini-ai-chatbot" passHref target="_blank">
                         <button className="flex mx-auto text-white bg-slate-500 border-0 py-2 px-8 focus:outline-none hover:bg-slate-400 rounded text-lg">
                             <DiGithubFull size={30}/>
                         </button>
